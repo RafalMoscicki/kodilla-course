3b4b63682357ca005aaea5853eee133f2e0d22dc
Rafal Moscicki <moscicki.rafal@gmail.com>
2021-07-26 17:08:30 +0200

    KOD.6.3 odd numbers test

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,51 @@
+package com.kodilla.testing.collection;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionTestSuite {
+
+    @BeforeEach
+    public void before() {
+        System.out.println("Test Case: begin");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+
+    @DisplayName("When empty list is created should return true after checking if list is empty")
+
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        List<Integer> result = oddNumbersExterminator.exterminate(new ArrayList<>());
+
+        //Then
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @DisplayName("When creating ArrayList should return List with odd numbers only")
+
+    @Test
+    void testOddNumbersExterminatorNormalList() {
+        //Given
+        Integer numbers[] = new Integer[] {5, 7, 0, 10, 12, 1};
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        List<Integer> result = oddNumbersExterminator.exterminate(Arrays.asList(numbers));
+
+        //When
+        List<Integer> expectedResult = new ArrayList<>();
+        expectedResult.add(0);
+        expectedResult.add(10);
+        expectedResult.add(12);
+
+        //Then
+        Assertions.assertEquals(result, expectedResult);
+    }
+}
