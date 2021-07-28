ec94a15cbbd7c9e0a527500afa458710b4a2cd20
Rafal Moscicki <moscicki.rafal@gmail.com>
2021-07-28 14:41:50 +0200

    KOD.6.4 shapes TDD tests

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Square.java
@@ -0,0 +1,41 @@
+package com.kodilla.testing.shape;
+
+public class Square implements Shape {
+
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public String getShapeName() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    public double getField() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Square square = (Square) o;
+
+        return Double.compare(square.side, side) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        long temp = Double.doubleToLongBits(side);
+        return (int) (temp ^ (temp >>> 32));
+    }
+
+    @Override
+    public String toString() {
+        return getShapeName() + "(" + side + ")";
+    }
+}
