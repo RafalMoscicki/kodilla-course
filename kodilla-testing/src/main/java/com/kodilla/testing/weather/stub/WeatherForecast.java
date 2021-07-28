aad0fb24dc7e724e54fd31030163f6a22f223ff1
rafal moscicki <moscicki.rafal@gmail.com>
2021-07-28 19:53:21 +0200

    KOD.6.5 mock median and average

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -1,7 +1,6 @@
 package com.kodilla.testing.weather.stub;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class WeatherForecast {
 
@@ -18,5 +17,37 @@ public class WeatherForecast {
             resultMap.put(temperature.getKey(), temperature.getValue() + 1.0);
         }
         return resultMap;
+    }
+
+    public double calculateAverage(Map<String, Double> temperatures) {
+        if (temperatures.isEmpty()) {
+            return 0;
+        } else {
+            double sum = 0;
+            for (Map.Entry<String, Double> temperature : temperatures.entrySet()) {
+                sum = sum + temperature.getValue();
+            }
+            return sum / (temperatures.size());
+        }
+    }
+
+    private List<Double> sort(Map<String, Double> numbers) {
+        List<Double> mapValues = new ArrayList<>(numbers.values());
+        Collections.sort(mapValues);
+        return mapValues;
+    }
+
+    public double calculateMedian(Map<String, Double> temperatures) {
+        if (temperatures.isEmpty()) {
+            return 0;
+        } else {
+            List<Double> sortedTemperatures = sort(temperatures);
+            int size = sortedTemperatures.size();
+            if (size % 2 == 0) {
+                return (sortedTemperatures.get((size / 2) - 1) + sortedTemperatures.get(size / 2)) / 2;
+            } else {
+                return sortedTemperatures.get((size / 2));
+            }
+        }
     }
 }
