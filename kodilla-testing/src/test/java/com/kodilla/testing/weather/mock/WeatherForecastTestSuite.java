a1c452bc800ec7ce7fc2fc18ee22dbbb6101bd7e
Rafal Moscicki <moscicki.rafal@gmail.com>
2021-07-29 17:54:35 +0200

    KOD.6.5 median and average tests fixed

--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -3,6 +3,8 @@ package com.kodilla.testing.weather.mock;
 import com.kodilla.testing.weather.stub.Temperatures;
 import com.kodilla.testing.weather.stub.WeatherForecast;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -16,49 +18,39 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class WeatherForecastTestSuite {
 
-    private Map<String, Double> createTemperaturesMap () {
-        Map<String, Double> temperaturesMap = new HashMap<>();
-        temperaturesMap.put("Rzeszow", 25.5);
-        temperaturesMap.put("Krakow", 26.2);
-        temperaturesMap.put("wroclaw", 24.8);
-        temperaturesMap.put("Warszawa", 25.2);
-        temperaturesMap.put("Gdansk", 26.1);
-        return temperaturesMap;
-    }
-
-    private double calculateAverageOfMock() {
-        Map<String, Double> tempMap = createTemperaturesMap();
-        double sum = 0;
-        for (Double temp : tempMap.values()) {
-            sum = sum + temp;
-        }
-        return sum / tempMap.size();
-    }
+    private static Map<String, Double> temperatures;
 
     @Mock
     private Temperatures temperaturesMock;
 
+    @BeforeAll
+    static void init() {
+        temperatures = createTemperaturesMap();
+    }
+
     @Test
     void testCalculateForecastWithMock() {
         //Given
-        when(temperaturesMock.getTemperatures()).thenReturn(createTemperaturesMap());
+        when(temperaturesMock.getTemperatures()).thenReturn(temperatures);
         WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
 
         //When
-        int quantityOfSensors = weatherForecast.calculateForecast().size();
+        Map<String, Double> result = weatherForecast.calculateForecast();
 
         //Then
-        Assertions.assertEquals(5, quantityOfSensors);
+        Assertions.assertEquals(5, result.size());
     }
 
     @Test
     void testCalculateMedianWithMock() {
         //Given
-        when(temperaturesMock.getTemperatures()).thenReturn(createTemperaturesMap());
+        when(temperaturesMock.getTemperatures()).thenReturn(temperatures);
+        Map<String, Double> temperatures = temperaturesMock.getTemperatures();
         WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
 
         //When
-        double result = weatherForecast.calculateMedian(temperaturesMock.getTemperatures());
+
+        double result = weatherForecast.calculateMedian(temperatures);
 
         //Then
         Assertions.assertEquals(25.5, result);
@@ -67,14 +59,23 @@ class WeatherForecastTestSuite {
     @Test
     void testCalculateAverageWithMock() {
         //Given
-        when(temperaturesMock.getTemperatures()).thenReturn(createTemperaturesMap());
+        when(temperaturesMock.getTemperatures()).thenReturn(temperatures);
         WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
-        temperaturesMock.getTemperatures();
 
         //When
         double result = weatherForecast.calculateAverage(temperaturesMock.getTemperatures());
-        double expectedResult = calculateAverageOfMock();
+
         //Then
-        Assertions.assertEquals(expectedResult, result);
+        Assertions.assertEquals(25.56, result);
+    }
+
+    private static Map<String, Double> createTemperaturesMap () {
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        temperaturesMap.put("Rzeszow", 25.5);
+        temperaturesMap.put("Krakow", 26.2);
+        temperaturesMap.put("wroclaw", 24.8);
+        temperaturesMap.put("Warszawa", 25.2);
+        temperaturesMap.put("Gdansk", 26.1);
+        return temperaturesMap;
     }
 }
