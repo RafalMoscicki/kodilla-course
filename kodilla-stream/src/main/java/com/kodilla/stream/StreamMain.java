e5357e001489c9163a13a2aaaa8923ff7d227897
Rafal Moscicki <moscicki.rafal@gmail.com>
2021-08-02 21:19:23 +0200

    KOD.7.1 poem beautifier

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,7 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.AdvPoemBeautifier;
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -20,6 +22,13 @@ public class StreamMain {
         expressionExecutor.executeExpression(3,4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3,4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3,4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("Hello world", (text -> "ABC" + text + "ABC"));
+        poemBeautifier.beautify("Hello world", (String::toLowerCase));
+        poemBeautifier.beautify("Hello world", (String::toUpperCase));
+        poemBeautifier.beautify("Hello world", (AdvPoemBeautifier::beautify));
 
     }
 }
