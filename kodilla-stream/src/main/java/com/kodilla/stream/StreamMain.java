c81a76d027579d06ea16355600103c512ea4969e
Rafal Moscicki <moscicki.rafal@gmail.com>
2021-08-02 20:27:00 +0200

    KOD.7.1 examples

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,25 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        // lambda expression
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        // method reference
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::divideAByB);
+
+    }
+}
