e50125130db26993e176806df35a0679fdef9341
Rafal Moscicki <moscicki.rafal@gmail.com>
2021-08-03 20:23:39 +0200

    KOD.7.3 Forum User

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,37 +1,21 @@
 package com.kodilla.stream;
-
-import com.kodilla.stream.beautifier.AdvPoemBeautifier;
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.forumUser.Forum;
+import com.kodilla.stream.forumUser.ForumUser;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
 
     public static void main(String[] args) {
 
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> forumUserMap = forum.getForumUserList().stream()
+                .filter(sex -> sex.getUserSex() == 'M')
+                .filter(age -> age.getUserBirthDate().getYear() < 2001)
+                .filter(post -> post.getUserPosts() >= 1)
+                .collect(Collectors.toMap(ForumUser::getUserId, user -> user));
 
-        // lambda expression
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        // method reference
-        expressionExecutor.executeExpression(3,4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3,4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3,4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3,4, FunctionalCalculator::divideAByB);
-
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-
-        poemBeautifier.beautify("Hello world", (text -> "ABC" + text + "ABC"));
-        poemBeautifier.beautify("Hello world", (String::toLowerCase));
-        poemBeautifier.beautify("Hello world", (String::toUpperCase));
-        poemBeautifier.beautify("Hello world", (AdvPoemBeautifier::beautify));
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        forumUserMap.entrySet()
+                .forEach(System.out::println);
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumUser/Forum.java
@@ -0,0 +1,29 @@
+package com.kodilla.stream.forumUser;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Forum {
+
+    private final List<ForumUser> forumUserList = new ArrayList<>();
+
+    public Forum() {
+        forumUserList.add(new ForumUser(123, "Adam", 'M',
+                LocalDate.of(1990, 5, 6), 10));
+        forumUserList.add(new ForumUser(124, "Kasia", 'W',
+                LocalDate.of(1991, 6, 22), 100));
+        forumUserList.add(new ForumUser(125, "Iwona", 'W',
+                LocalDate.of(1995, 7, 30), 5));
+        forumUserList.add(new ForumUser(126, "Alex", 'M',
+                LocalDate.of(2000, 12, 18), 185));
+        forumUserList.add(new ForumUser(127, "Piotr", 'M',
+                LocalDate.of(1980, 1, 4), 20));
+        forumUserList.add(new ForumUser(128, "Iza", 'W',
+                LocalDate.of(1985, 2, 16), 30));
+    }
+
+    public List<ForumUser> getForumUserList() {
+        return new ArrayList<>(forumUserList);
+    }
+}
